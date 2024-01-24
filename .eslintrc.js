4e7b479d2ef0da56949531a3095f1ec74af55196
Oleksandr Ponomarenko <oon@conscensia.com>
2024-01-24 16:21:29 +0200

    Added ESLint & Prettier to the project config

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+      },
+    },
+  ],
+  plugins: ['react', 'import', 'jsx-a11y', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/prop-types': 0,
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': 1,
+    quotes: ['error', 'single'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['internal', 'parent', 'sibling', 'index'],
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
